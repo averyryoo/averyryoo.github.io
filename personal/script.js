562f5f1892a739f3ae4f0c9622c9ca4e6f0e3a74
Avery Ryoo <averyryoo@outlook.com>
2023-08-15 17:53:12 -0400

    deleted unnecessary files

--- personal/script.js
+++
@@ -1,9 +0,0 @@
-// Initialize the map
-var map = L.map('map').setView([0, 0], 2); // Center the map at (latitude: 0, longitude: 0) with zoom level 2
-
-// Add a tile layer (OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// Add markers or other map features as needed
